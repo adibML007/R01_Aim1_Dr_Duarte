78ef93cf9263f66c74c1dad00e98e9de21abf732
Muhammad Adib Uz Zaman <a_u_z_ipe@yahoo.com>
2022-06-24 03:43:04 -0400

    Add files via upload

---
+++ geocoding_steps_in_R.R
@@ -0,0 +1,12 @@
+library(dplyr, warn.conflicts = FALSE)
+library(tidygeocoder)
+library(readr)
+rm(list=ls())
+provider_list <- read_csv("C:/Users/rafsz/Desktop/Provider_FL_new.csv")
+View(provider_list)
+provider_list$add<-paste(provider_list$Address,
+                         provider_list$City,
+                         provider_list$State,sep=",")
+#X = provider_list
+#Y = geocode(X, address = add, method = "arcgis", verbose = TRUE)
+#write.csv(Y,'geocoded_address_FL_final.csv',row.names=FALSE)
